9f8d03d7f74a589609547a1d79557444ee153c59
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2024-11-22 14:02:12 +0530

    feat: :sparkles: implement providers

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ReactQueryProvider, ThemeProvider } from "@/components/providers";
 
 const geistSans = localFont({
 	src: "./fonts/GeistVF.woff",
@@ -28,7 +29,9 @@ export default function RootLayout({
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
-				{children}
+				<ReactQueryProvider>
+					<ThemeProvider>{children}</ThemeProvider>
+				</ReactQueryProvider>
 			</body>
 		</html>
 	);
