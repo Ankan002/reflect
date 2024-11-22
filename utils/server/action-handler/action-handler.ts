293deffe5ddc09561f1371f7da0f9cd139118dc9
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2024-11-22 16:08:11 +0530

    feat: :sparkles: add action handler

---
+++ utils/server/action-handler/action-handler.ts
@@ -0,0 +1,44 @@
+import { JsonWebTokenError } from "jsonwebtoken";
+import type { ActionController } from "@/types/api";
+import type { APIError } from "@/types/error";
+
+export const actionHandler =
+	<D = void, A = Record<string, never>>(
+		controllerFunc: ActionController<D, A>,
+	): ActionController<D, A> =>
+	async (args?: A) => {
+		try {
+			return await Promise.resolve(controllerFunc(args));
+		} catch (error) {
+			console.log(error);
+			if (error instanceof JsonWebTokenError) {
+				return {
+					success: false,
+					code: 401,
+					error: error.message,
+				};
+			}
+
+			if (error instanceof APIError) {
+				return {
+					success: false,
+					code: error.code,
+					error: error.message,
+				};
+			}
+
+			if (error instanceof Error) {
+				return {
+					success: false,
+					code: 400,
+					error: error.message,
+				};
+			}
+
+			return {
+				success: false,
+				code: 500,
+				error: "Internal Server Error!!",
+			};
+		}
+	};
