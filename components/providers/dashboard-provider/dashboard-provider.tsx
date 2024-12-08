59e38251567ce9808877718c857d7d72bb909a24
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2024-12-08 13:42:28 +0530

    feat: :sparkles: add loading skeleton

--- components/providers/dashboard-provider/dashboard-provider.tsx
+++ components/providers/dashboard-provider/dashboard-provider.tsx
@@ -6,6 +6,8 @@ import {
 	SidebarProvider,
 	SidebarTrigger,
 } from "@/components/ui/sidebar";
+import { useDashboardProvider } from "./hook";
+import { Skeleton } from "@/components/ui/skeleton";
 
 interface Props {
 	children: React.ReactNode;
@@ -15,13 +17,24 @@ interface Props {
 const DashboardProvider = (props: Props) => {
 	const { children, heading } = props;
 
+	const { isAppLoaded, isUserLoading, user } = useDashboardProvider();
+
 	return (
 		<SidebarProvider>
 			<CommonSidebar />
 			<SidebarInset>
-				<div className="w-full flex items-center my-2 px-2">
-					<SidebarTrigger className="text-primary" />
-					<p className="text-primary text-lg ml-1">{heading}</p>
+				<div className="w-full flex justify-between items-center my-2 px-2">
+					<div className="flex items-center">
+						<SidebarTrigger className="text-primary" />
+						<p className="text-primary text-lg ml-1">{heading}</p>
+					</div>
+					<div>
+						{isUserLoading || !isAppLoaded || !user ? (
+							<Skeleton className="h-10 w-10 rounded-full bg-secondary border border-foreground" />
+						) : (
+							<></>
+						)}
+					</div>
 				</div>
 				{children}
 			</SidebarInset>
