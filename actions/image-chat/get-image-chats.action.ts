73dd21c3d3797fa9a40e0e4515867047a84f67c6
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2024-12-09 15:18:04 +0530

    feat: :sparkles: add get image chats action

---
+++ actions/image-chat/get-image-chats.action.ts
@@ -0,0 +1,36 @@
+"use server";
+
+import {
+	actionHandler,
+	getPrismaClient,
+	verifyAuthToken,
+} from "@/utils/server";
+import { image_gen_chat } from "@prisma/client";
+
+interface Response {
+	chats: Array<Pick<image_gen_chat, "id" | "name">>;
+}
+
+export const getImageChatsAction = actionHandler<Response>(async () => {
+	const { id } = await verifyAuthToken();
+
+	const prisma = getPrismaClient();
+
+	const chats = await prisma.image_gen_chat.findMany({
+		where: {
+			user_id: id,
+		},
+		select: {
+			id: true,
+			name: true,
+		},
+	});
+
+	return {
+		success: true,
+		code: 200,
+		data: {
+			chats,
+		},
+	};
+});
--- actions/image-chat/index.ts
+++ actions/image-chat/index.ts
@@ -1,1 +1,2 @@
 export * from "./create-image-chat.action";
+export * from "./get-image-chats.action";
