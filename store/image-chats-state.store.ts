69b867dcd4deb92427172ae915bff6ef5c68ae31
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2024-12-09 15:34:36 +0530

    feat: :sparkles: add image chats store

---
+++ store/image-chats-state.store.ts
@@ -0,0 +1,20 @@
+import { image_gen_chat } from "@prisma/client";
+import { create } from "zustand";
+
+interface ImageChatsStore {
+	chats: Pick<image_gen_chat, "id" | "name">[];
+	setChats: (newChats: Pick<image_gen_chat, "id" | "name">[]) => void;
+	addChat: (newChat: Pick<image_gen_chat, "id" | "name">) => void;
+	removeChat: (chatId: string) => void;
+}
+
+export const useImageChatsStateStore = create<ImageChatsStore>()((set) => ({
+	chats: [],
+	setChats: (newChats) => set({ chats: newChats }),
+	addChat: (newChat) =>
+		set((state) => ({ chats: [...state.chats, newChat] })),
+	removeChat: (chatId) =>
+		set((state) => ({
+			chats: state.chats.filter((chat) => chat.id !== chatId),
+		})),
+}));
--- store/index.ts
+++ store/index.ts
@@ -3,3 +3,4 @@ export * from "./auth-state.store";
 export * from "./app-load-state.store";
 export * from "./user.store";
 export * from "./user-load.store";
+export * from "./image-chats-state.store";
